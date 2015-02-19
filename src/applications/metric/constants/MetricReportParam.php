0b05fe5313a427822b58eb29c05229d0cda0cee3
Alexander Obuhovich <aik.bold@gmail.com>
2015-02-19 21:17:32 +0200

    Initial Metrics application version

---
+++ src/applications/metric/constants/MetricReportParam.php
@@ -0,0 +1,14 @@
+<?php
+
+final class MetricReportParam extends MetricConstants {
+
+  const GRANULARITY_DAY = 1;
+  const GRANULARITY_WEEK = 2;
+  const GRANULARITY_MONTH = 3;
+  const GRANULARITY_YEAR = 4;
+
+  const BREAKDOWN_NONE = 1;
+  const BREAKDOWN_USERS = 2;
+  const BREAKDOWN_REPOSITORIES = 3;
+
+}
