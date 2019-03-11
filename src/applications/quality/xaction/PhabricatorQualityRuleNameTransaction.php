f0ba0c327a3287ecb8cc86664f056ab8ba532c6e
Alexander Obuhovich <aik.bold@gmail.com>
2019-03-11 10:13:03 +0200

    Initial "Quality" application version

---
+++ src/applications/quality/xaction/PhabricatorQualityRuleNameTransaction.php
@@ -0,0 +1,44 @@
+<?php
+
+final class PhabricatorQualityRuleNameTransaction
+  extends PhabricatorQualityRuleTransactionType {
+
+  const TRANSACTIONTYPE = 'quality.rule.name';
+
+  public function generateOldValue($object) {
+    return $object->getName();
+  }
+
+  public function applyInternalEffects($object, $value) {
+    $object->setName($value);
+  }
+
+  public function getTitle() {
+    return pht(
+      '%s changed the name of the Quality Rule from %s to %s.',
+      $this->renderAuthor(),
+      $this->renderOldValue(),
+      $this->renderNewValue());
+  }
+
+  public function getTitleForFeed() {
+    return pht(
+      '%s changed the name of %s from %s to %s.',
+      $this->renderAuthor(),
+      $this->renderObject(),
+      $this->renderOldValue(),
+      $this->renderNewValue());
+  }
+
+  public function validateTransactions($object, array $xactions) {
+    $errors = array();
+
+    if ($this->isEmptyTextTransaction($object->getName(), $xactions)) {
+      $errors[] = $this->newRequiredError(
+        pht('Quality rules must have a name.'));
+    }
+
+    return $errors;
+  }
+
+}
