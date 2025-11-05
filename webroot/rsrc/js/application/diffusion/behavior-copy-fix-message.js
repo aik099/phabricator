bd85a5f9243ffd371cab158e13cb8bb9468a0c88
Alex <aik099@users.noreply.github.com>
2025-11-05 13:35:55 +0200

    Use native clipboard API instead of a ZeroClipboard

--- webroot/rsrc/js/application/diffusion/behavior-copy-fix-message.js
+++ webroot/rsrc/js/application/diffusion/behavior-copy-fix-message.js
@@ -4,7 +4,6 @@
  *           javelin-dom
  *           javelin-stratcom
  *           javelin-util
- *           zeroclipboard
  */
 
 JX.behavior('diffusion-copy-fix-message', function(config) {
@@ -12,31 +11,22 @@ JX.behavior('diffusion-copy-fix-message', function(config) {
       sigil = 'copy-fix-message',
       copy_link = JX.DOM.scry(document.body, 'a', sigil).pop();
 
-  ZeroClipboard.config({
-    hoverClass: 'phabricator-action-view-hover'
-  });
-
-  zc_client = new ZeroClipboard(copy_link.parentElement);
-
-  zc_client.on('ready', function (readyEvent) {
-    zc_client.on('copy', function (event) {
-      var node_data = JX.Stratcom.getData(copy_link);
-      zc_client.setText(node_data['fix-message']);
-    });
-
-    zc_client.on('aftercopy', function (event) {
-      copy_link.innerHTML += ' <strong>(copied)</strong>';
-
-      setTimeout(function () {
-        copy_link.innerHTML = copy_link.innerHTML.replace(/ <strong>\(copied\)<\/strong>$/, '');
-      }, 2000);
-    });
-  });
-
   JX.Stratcom.listen(
     'click',
     sigil,
     function (e) {
       e.kill();
+
+      var node_data = JX.Stratcom.getData(copy_link);
+
+      navigator.clipboard
+        .writeText(node_data['fix-message'])
+        .then(() => {
+          copy_link.innerHTML += ' <strong>(copied)</strong>';
+
+          setTimeout(function () {
+            copy_link.innerHTML = copy_link.innerHTML.replace(/ <strong>\(copied\)<\/strong>$/, '');
+          }, 2000);
+        });
     });
 });
